1c4eeaa3e37a0cc63d31be18f198e299ed258e97
Aidar Turkenov <a.k.turken0v@gmail.com>
2023-01-21 18:19:06 +0600

    test github publish

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('./README.md', 'r', encoding='utf-8') as file:
 
 setup(
     name='pyservices',
-    version='0.0.1',
+    version='0.0.1.1',
     author='Aidar Turkenov',
     author_email='a.k.turken0v@gmail.com',
     description='',
